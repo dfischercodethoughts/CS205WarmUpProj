8c0844da2f2400291cdfb8d24b70e7d3cbc29cfe
David <david.fischer@uvm.edu>
2019-02-11 14:38:24 -0600

    finalized db structure. todo: import csv files

--- init_pokedb.py
+++ init_pokedb.py
@@ -18,19 +18,17 @@ executor = con.cursor()
 
 exec_string = []
 
-exec_string.append( "create table location_records (" +
+exec_string.append( "create table location_records_no_fk (" +
     "id integer primary key, " +'location_name_1 text not null, location_name_2 text not null, location_name_3 text not null, location_4 text not null, location_5 text not null, location_6 text not null,' + 
     'pokemon_name text not null, ' + 'evolved int not null default 0, '+
     'evolved_from text, ' + 'evolution_level integer );')
 
 
-exec_string.append("""create table pokemon (name text primary key, 
+exec_string.append("""create table pokemon_no_fk (name text primary key, 
     hp integer not null, 
     location_record_id integer, 
     type1 text not null, 
-    type2 text, 
-    primary_attack text not null, 
-    secondary_attack text);"""
+    type2 text);"""
 )
 exec_string.append("""create table locations (name text primary key, 
     description text, 
@@ -54,15 +52,40 @@ exec_string.append( """create table attacks (name text primary key,
     foreign key (location_name) references locations(name)
     );""")
 
-exec_string.append( """alter table pokemon add primary_attack foreign key (primary_attack) references attacks(name),
-    foreign key (secondary_attack) references attacks(name),
-    foreign key (location_record_id) references location_records(id)
-    );""")
+exec_string.append("create table pokemon (" +
+        "name text primary key," + 
+    "hp integer not null, " +
+   " type1 text not null, " +
+    "type2 text," +
+    "primary_attack text," + 
+    "secondary_attack text," +
+    "location_record_id integer," +
+    "foreign key(primary_attack) references attacks(name)," +
+    "foreign key(secondary_attack) references attacks(name)," +
+    "foreign key(location_record_id) references locations(name));")
+
+exec_string.append("create table location_records(" + 
+    "id integer primary key, " +'location_1 text not null, location_2 text not null, location_3 text not null, location_4 text not null, location_5 text not null, location_6 text not null,' + 
+    'pokemon_name text not null, ' + 'evolved int not null default 0, '+
+    'evolved_from text, ' + 'evolution_level integer,' + 'foreign key(location_1) references locations(name),'  + 
+    'foreign key(location_2) references locations(name),' + 'foreign key(location_3) references locations(name),'  + 
+    'foreign key(location_4) references locations(name),'  + 'foreign key(location_5) references locations(name),'  + 
+    'foreign key(location_6) references locations(name),' +
+    'foreign key(pokemon_name) references pokemon(name));')
+
+exec_string.append('drop table location_records_no_fk;')
+exec_string.append('drop table pokemon_no_fk;')
+
 
 
 for i in exec_string:
     print(i)
     executor.execute(i)
+    executor.execute("select * from sqlite_master")
+    out = executor.fetchall()
+    print("output:")
+    for o in out:
+        print(o)
 
 con.commit()
 
@@ -70,7 +93,7 @@ con.commit()
 
 #executor.execute('select * from sqlite_master where type="table";')
 
-table_name_verify = executor.fetch_all()
+table_name_verify = executor.fetchall()
 for table_name in table_name_verify:
     print(table_name)
 
