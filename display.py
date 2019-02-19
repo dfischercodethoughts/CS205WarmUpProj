e1f5a006ad0b5643c8a2bd835ba771aa17181184
David <david.fischer@uvm.edu>
2019-02-18 22:52:05 -0600

    fixed bugs in pokemon evolutions sql statement

--- display.py
+++ display.py
@@ -15,26 +15,26 @@ def display_key_and_value(key,value,left_size=15,right_size=15):
     return False
 
 def display_pokemon(dictionary):
-    first_last_str = ""
-    first_last_str += "-" * 34
+    first_last_str = " "
+    first_last_str += "_" * 32
     print(first_last_str)
-    display_key_and_value("name",dictionary.get("name"))
-    display_key_and_value("type 1",dictionary.get("type1"))
+    display_key_and_value("name",dictionary.get("name"),16,15)
+    display_key_and_value("type 1",dictionary.get("type1"),16,15)
     if dictionary.get("type_2") != "":
-        display_key_and_value("type 2",dictionary.get("type2"))
-    display_key_and_value("Health",dictionary.get("hp"))
-    display_key_and_value("primary attack",dictionary.get("primary_attack"))
-    display_key_and_value("secondary attack",dictionary.get("secondary_attack"))
+        display_key_and_value("type 2",dictionary.get("type2"),16,15)
+    display_key_and_value("Health",dictionary.get("hp"),16,15)
+    display_key_and_value("primary attack",dictionary.get("primary_attack"),16,15)
+    display_key_and_value("secondary attack",dictionary.get("secondary_attack"),16,15)
     if dictionary.get("evolution_level") != 0:
-        display_key_and_value("evolves at level",dictionary.get("evolution_level"))
+        display_key_and_value("evolves at level",dictionary.get("evolution_level"),16,15)
     print(first_last_str)
 
 def display_pokemon_evolutions(list_of_dics, poke_name):
     header = poke_name[0].upper() + poke_name[1:len(poke_name)] + " Evolutions"
-    print("{:^50}".format(header))
-    print("-" * 52)
+    print("\n\n{:^50}".format(header))
+    print("_" * 52)
     for dic in list_of_dics:
-        print("-" * 52)
+        print(" " + "_" * 51)
         #print(dic)
         if dic.get("is_parent"):
             display_key_and_value("Parent Poke"," " +dic.get('poke_name'),20,30)
@@ -43,22 +43,22 @@ def display_pokemon_evolutions(list_of_dics, poke_name):
         if dic.get("is_item_used") != None and dic.get("is_item_used") != 0:
             display_key_and_value("Item used",dic.get("item_used"),20,30)
         if dic.get("is_bred") != None and dic.get("is_bred") != 0:
-            display_key_and_value("","By breeding.",20,30)
+            display_key_and_value("Acquired"," by breeding.",20,30)
         if dic.get("is_evolve") != None and dic.get("is_evolve") != 0:
-            display_key_and_value("", "By evolution.",20,30)
+            display_key_and_value("Acquired", " by evolution.",20,30)
         if dic.get("is_traded") != None and dic.get("is_traded") != 0:
-            display_key_and_value("","By trading.",20,30)
-        if dic.get("additional_requirements") != 0 and dic.get("additional_requirements") != None and dic.get("additional_requirements") != "":
-            display_key_and_value("Additional reqs:",dic.get("additional_requirements"))
-        print("-" * 52)
+            display_key_and_value("Acquired"," by trading.",20,30)
+        if dic.get("additional_requirements") != "0" and dic.get("additional_requirements") != 0 and dic.get("additional_requirements") != None and dic.get("additional_requirements") != "":
+            display_key_and_value("Additional reqs"," " +dic.get("additional_requirements"),20,30)
+        print(" " + "_" * 51)
     print("No more associated evolutions")
 
 def display_pokemon_attacks(dic,pokemon_name):
     header = pokemon_name[0].upper() + pokemon_name[1:len(pokemon_name)].lower()
     header = header + " Attacks"
 
-    print("{:^50}".format(header))
-    print("-" * 52)
+    print("\n\n{:^50}".format(header))
+    print(" " +"_" * 51)
     if dic.get("primary_att_name") != "":
         display_key_and_value("Primary attack",dic.get("primary_att_name"),20,30)
         if dic.get("primary_att_damage") != None and str(dic.get("primary_att_damage")) != "" and dic.get("primary_att_damage") != 0:
@@ -74,7 +74,7 @@ def display_pokemon_attacks(dic,pokemon_name):
         display_key_and_value("Effect",dic.get("primary_att_effect"),20,30)
     else:
         print("No primary attack found.")
-    print("\n")
+    print(" " +"_" * 51)
     if dic.get("secondary_att_name") != "":
         display_key_and_value("Secondary attack",dic.get("secondary_att_name"),20,30)
         if dic.get("secondary_att_damage") != None and str(dic.get("secondary_att_damage")) != "" and dic.get("secondary_att_damage") != 0:
@@ -90,15 +90,34 @@ def display_pokemon_attacks(dic,pokemon_name):
         display_key_and_value("Effect",dic.get("secondary_att_effect"),20,30)
     else:
         print("No secondary attack found.")
-    print("-" * 52)
+    print(" " + "_" * 51)
+    
 def display_pokemon_locations(location_array,pokemon_name):
+    
     if location_array:
-        to_display = pokemon_name[0].upper() + " can be found in:"
+        to_display = pokemon_name[0].upper() + pokemon_name[1:len(pokemon_name)] + " can be found in:"
+        print("\n{:<25}".format(to_display))
+        print(" " + "_" * 40)
         for location in location_array:
-            to_display += "\n\t{:<15}".format(location)
+            display_key_and_value("",location[0],7,33)
+            
     else:
         to_display = "No locations found for that pokemon."
-    print(to_display)
+    print(" " + "_" * 40)
+
+def display_pokemon_in_location(pokemon_array,location_name):
+
+    if pokemon_array:
+        to_display = "\n" + location_name[0].upper() + location_name[1:len(location_name)] + " contains the following pokemon:"
+        print(to_display)
+        print(" " + "_" * 40)
+        for pokemon in pokemon_array:
+            poke_name = pokemon[0][0].upper()
+            poke_name += pokemon[0][1:len(pokemon[0])]
+            display_key_and_values("",poke_name,5,35)
+        print(" " + "_"*40)
+    else:
+        print("No pokemon found for that location.")
 
 def list_table(table_name):
     table_name = table_name.lower()
@@ -108,41 +127,40 @@ def list_table(table_name):
         results = menu_options.execute(sql)
         if results:
             print("ATTACKS TABLE")
-            print("-"*35)
-            header_str = "|{:^10}|\n".format("name")
-            header_str += "|{:^6}|".format("damage")
-            header_str += "{:^10}|".format("targets")
-            header_str += "{:^3}|\n".format("PP")
-            header_str+= "|{:^8}|".format("accuracy")
-            header_str += "{:^20}|\n".format("found in")
-            header_str += "|{:^30}|\n".format("effect")
-            print(header_str)
-            print("-"*35)
 
             for attack in results:
-                print("-" * 35)
-                row = "\n|{:<15}|\n".format("Name: " + attack[0])
-                row += "|{:<10}|".format("Damage: " + str(attack[1]))
-                row += "{:<15}|".format("Targets: " + attack[3])
-                row += "{:<5}|\n".format("PP: " + str(attack[4]))
-                row += "|{:<11}|".format("Acc: " + str(attack[5]))
-                row += "{:<20}|\n".format("Found: " + attack[6])
-                row += "|{:<30}|\n".format("Effect: " + attack[2])
-                print(row)
-                print("-" * 35)
+                print(" " + "_" * 51)
+                pokename = attack[0][0].upper()
+                pokename += attack[0][1:len(attack[0])]
+                display_key_and_value("Name",pokename,10,40)
+                if attack[1] != None and attack[1] != 0 and attack[1] != "":
+                    display_key_and_value("Damage",str(attack[1]),10,40)
+                else:
+                    display_key_and_value("No damage","See effect",10,40)
+                display_key_and_value("Targets", attack[3],10,40)
+                display_key_and_value("PP",str(attack[4]),10,40)
+                if attack[5] != 0 and attack[5] != "":
+                    display_key_and_value("Accuracy", str(attack[5]),10,40)
+                if attack[6] != None and attack[6] != 0 and attack[6] != "" and attack[6] != "none":
+                    display_key_and_value("Found in",attack[6],10,40)
+                if attack[2] != None and attack[2] !=0 and attack[2] != "":
+                    display_key_and_value("Effect",attack[2],10,40)
+                print(" " + "_" * 51)
         else:
             raise validate.Input_Error("No attacks. Perhaps the db has not been set up yet.")
     elif table_name == "locations":
         sql = "select name, description from locations;"
         results = menu_options.execute(sql)
         if results:
-            print("LOCATIONS")
-            print("-"*51)
-            header_str = "\n|{:^25}::".format("Name")
-            header_str += "{:^25}|\n".format("Description")
+            print("{:^55}" . format("LOCATIONS"))
+            print(" " + "_"*55)
+            header_str = "\n|{:^25}:".format("name")
+            header_str += "{:^30}|".format("description")
             print(header_str)
+            print("|{:^56}|".format(""))
             for location in results:
-                display_key_and_value(location[0], location[1],25,25)
+                display_key_and_value(location[0],location[1],25,30)
+            print(" " + "_"*55)
 
         else:
             raise validate.Input_error("No locations. Perhaps the db has not been set up yet.")
@@ -160,24 +178,21 @@ def list_table(table_name):
         results = menu_options.execute(sql)
         if results:
             print("EVOLUTIONS TABLE")
-            print("-"*40)
-            header_str = "\n|{:^10}|\n".format("parent poke")
-            header_str += "|{^:10}|".format("child poke")
-            header_str += "{^:6}|".format("evolved?")
-            header_str += "{^:6}|".format("item?")
-            header_str+= "{^:16}|\n".format("item name")
-            header_str+= "{^:8}|".format("traded?")
-            header_str += "{^:5}|\n".format("bred?")
-            header_str+= "|{^:25}|\n".format("additional notes")
-            print(header_str)
-            print("-"*20)
+            for evolution in results:
+                print(" " + "_"*45)
+                display_key_and_value("Parent Poke",evolution[0],15,30)
+                display_key_and_value("Child Poke", evolution[1],15,30)
+                if evolution[2] == 1:
+                    display_key_and_value("", "Evolves through level up",15,30)
+                if evolution[3] == 1 and evolution[4] != None:
+                    display_key_and_value("","Transforms by using " + evolution[4],15,30)
+                if evolution[5] == 1:
+                    display_key_and_value("","Transforms through trading", 15, 30)
+                if evolution[6] == 1:
+                    display_key_and_value("","Transforms through breeding",15,30)
+                if evolution[7] != None and evolution[7] != 0 and evolution[7] != "":
+                    display_key_and_value("Additional reqs",evolution[7],15,30)
+                print(" " +"_"*45)
+        else:
+                raise validate.Input_Error("No evolutions. Perhaps the db has not been set up yet.")
 
-            for attack in results:
-                header_str = "\n|{:^10}|\n".format(results[0][0])
-                header_str += "|{^:10}|".format(results[0][1])
-                header_str += "{^:6}|".format(str(results[0][2]))
-                header_str += "{^:6}|".format(str(results[0][3]))
-                header_str+= "{^:16}|\n".format(results[0][4])
-                header_str+= "{^:8}|".format(str(results[0][5]))
-                header_str += "{^:5}|\n".format(str(results[0][6]))
-                header_str+= "|{^:25}|\n".format(results[0][7])
